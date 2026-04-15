fd0ddae6aa7fc5f0e692bfd2817f2c6ed61476e8
PriyankarMondal <mpriyankar04@gmail.com>
2026-04-15 16:36:08 +0530

    Switch BASE_URL to production endpoint

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:8000";
-// const BASE_URL = "https://learn-and-earn-contest-backend.onrender.com";
+// const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://learn-and-earn-contest-backend.onrender.com";
 
 
 // generic request function
